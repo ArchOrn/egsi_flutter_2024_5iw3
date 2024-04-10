4df76a6d7cabcf7302b2d6aed8360c499ee3d714
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-04-10 15:09:43 +0200

    Demo with StatefulWidget and screen swap via BottomNavigationBar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_5iw3/calendar_screen.dart';
+import 'package:flutter_5iw3/favorites_screen.dart';
+
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _screens = [
+    const FavoritesScreen(),
+    const CalendarScreen(),
+  ];
+
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +41,20 @@ class Home extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          'Hello',
-          style: Theme.of(context).textTheme.displayLarge,
-        ),
-      ),
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blue,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             label: 'Favorites',
             icon: Icon(
               Icons.favorite,
-              color: Colors.blue,
             ),
           ),
           BottomNavigationBarItem(
