c389b5eb61710416efce7cad6dfb4ed072cd08c0
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-04-10 16:09:34 +0200

    Rollback to blank Scaffold with red square for exercise

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_5iw3/red_square.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,83 +9,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Color _backgroundColor = Colors.white;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        elevation: 1,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.airplanemode_on,
-            color: Colors.white,
-          ),
-        ),
-        title: const Text(
-          'My Title',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-      backgroundColor: _backgroundColor,
-      body: Center(
-        child: Square(
-          onTap: () {
-            setState(() {
-              _backgroundColor = _backgroundColor == Colors.white ? Colors.black : Colors.white;
-            });
-          },
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
-        items: const [
-          BottomNavigationBarItem(
-            label: 'Favorites',
-            icon: Icon(
-              Icons.favorite,
-            ),
-          ),
-          BottomNavigationBarItem(
-            label: 'Today',
-            icon: Icon(
-              Icons.calendar_today,
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        shape: const CircleBorder(),
-        onPressed: () {},
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-}
-
-class Square extends StatelessWidget {
-  final VoidCallback onTap;
-
-  const Square({super.key, required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.red,
-      ),
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: RedSquare(),
     );
   }
 }
