a7d37cba1d135ea240bd875e57bba3f90577c873
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-04-12 09:10:23 +0200

    Demo with SingleChildScrollView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_5iw3/red_square.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                const RedSquare(),
+                ...List.generate(100, (index) {
+                  return const Column(
+                    children: [
+                      SizedBox(height: 10),
+                      RedSquare(),
+                    ],
+                  );
+                })
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
