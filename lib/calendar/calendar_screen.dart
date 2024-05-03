89d525d0a1298649d5091553733821705231cbe2
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-03 11:01:06 +0200

    Demo with pop()

--- lib/calendar/calendar_screen.dart
+++ lib/calendar/calendar_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CalendarScreen extends StatelessWidget {
-  static void navigateTo(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(
+  static Future<void> navigateTo(BuildContext context) {
+    return Navigator.of(context).push(MaterialPageRoute(
       builder: (context) {
         return const CalendarScreen();
       },
@@ -13,27 +13,25 @@ class CalendarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SizedBox(
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Calendar screen',
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            // const SizedBox(height: 10),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     // TODO
-            //   },
-            //   child: const Text('Go!'),
-            // ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('<- Back'),
+            ),
           ],
         ),
       ),
