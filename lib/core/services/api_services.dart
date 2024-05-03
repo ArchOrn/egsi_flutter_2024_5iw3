bff939fa579b3a58a740c65483e3dff523c14cd5
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-03 09:15:45 +0200

    Demo with basic api calls using http

---
+++ lib/core/services/api_services.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter_5iw3/core/models/post.dart';
+import 'package:http/http.dart' as http;
+
+class ApiServices {
+  static Future<List<Post>> getPosts() async {
+    try {
+      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+      await Future.delayed(const Duration(seconds: 1));
+      if (response.statusCode < 200 || response.statusCode >= 400) {
+        throw Error();
+      }
+
+      final data = json.decode(response.body) as List<dynamic>;
+      return data.map((e) => Post.fromJson(e)).toList();
+    } catch (error) {
+      log('An error occurred while retrieving posts.', error: error);
+      rethrow;
+    }
+  }
+}
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_5iw3/core/models/post.dart';
+import 'package:flutter_5iw3/core/services/api_services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Post> _posts = [];
+  bool _loading = false;
+  Error? _error;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _loading = true;
+    });
+
+    ApiServices.getPosts().then((data) {
+      _posts = data;
+    }).catchError((error) {
+      _error = error;
+    }).whenComplete(() {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
+      body: _loading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : (_error != null
+              ? Center(
+                  child: Text(
+                    _error.toString(),
+                    style: const TextStyle(
+                      color: Colors.red,
+                    ),
+                  ),
+                )
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    final post = _posts[index];
+                    return ListTile(
+                      title: Text(post.title),
+                      subtitle: Text(post.body),
+                    );
+                  },
+                  itemCount: _posts.length,
+                )),
     );
   }
 }
