6926545b628ccc545ce271bbc5041c2b0bd74bd7
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-03 08:25:33 +0200

    Reset blank app

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_5iw3/home_screen.dart';
+import 'package:flutter_5iw3/home/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Home(),
+      home: const HomeScreen(),
       theme: ThemeData(
         textTheme: const TextTheme(
           displayLarge: TextStyle(
