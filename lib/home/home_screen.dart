0e0c9386b329a64b3c507bc60e047e7f315a5442
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-03 10:42:59 +0200

    Reset with blank home screen with text

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,63 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_5iw3/core/services/api_services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.white,
-      body: FutureBuilder(
-        future: ApiServices.getPhotos(),
-        builder: (context, snapshot) {
-          final loading = snapshot.connectionState == ConnectionState.waiting;
-          if (loading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                snapshot.error.toString(),
-                style: const TextStyle(
-                  color: Colors.red,
-                ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Home screen',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
               ),
-            );
-          }
-
-          return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
             ),
-            itemBuilder: (context, index) {
-              final photo = snapshot.data![index];
-              return Stack(
-                children: [
-                  Image.network(photo.url),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      color: Colors.black.withOpacity(.2),
-                      child: Text(
-                        photo.title,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            },
-            itemCount: snapshot.hasData ? snapshot.data!.length : 0,
-          );
-        },
+          ],
+        ),
       ),
     );
   }
